88391dfb00ba815c4ece6b6572e88b974a572683
Elysiayn <wendy.lemus.wl@gmail.com>
2021-02-09 17:13:58 -0500

    added structure for upload page

---
+++ models/Upload.js
@@ -0,0 +1,8 @@
+// var admin = require('firebase-admin');
+// var app = admin.initializeApp();
+
+
+// app.use((req, res) => {
+//     res.status(404).render("404page", {title:"404 not found",
+//     uploadstyle: `<link rel="stylesheet" href="/css/customstyle.css">`});
+// });
