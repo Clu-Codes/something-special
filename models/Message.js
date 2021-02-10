d0fabd44de4b5dc2cf952fa0e393038d25eb0f03
Mila Decker <milacdecker@gmail.com>
2021-02-09 19:00:57 -0500

    added category, message, post, and user models

---
+++ models/Message.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Message extends Model {}
+
+Message.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        message_text: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize, 
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Message;
