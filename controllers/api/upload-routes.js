c54eed23169da66782080a5aaf64d10cd9f334c4
Elysiayn <wendy.lemus.wl@gmail.com>
2021-02-09 20:02:30 -0500

    modified upload route

--- controllers/api/upload-routes.js
+++ controllers/api/upload-routes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
-const { Upload } = require('../../models');
 const sequelize = require('../../config/connection');
+const { Upload } = require('../../models');
 
+// GET /api/upload
+router.get('/', (req, res) => {});
+
+// GET /api/upload/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/upload
+router.post('/', (req, res) => {});
+
+// PUT /api/upload/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/upload/1
+router.delete('/:id', (req, res) => {});
 
 
 module.exports = router;
