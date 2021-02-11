b189ecb121b89ed67ec20fd1ca2a17faa7d278b0
Elysiayn <wendy.lemus.wl@gmail.com>
2021-02-10 20:08:01 -0500

    upload storage bug found, upload fails to initialize

--- public/javascript/upload-file.js
+++ public/javascript/upload-file.js
@@ -5,15 +5,16 @@ var  fileButton = document.getElementById('fileButton');
 // listen for file selection
 fileButton.addEventListener('change', function(e){
     // get file
-    var file = e.target.files[0];
-    // create storage ref
-    var storageRef = firebase.storage().ref('special_pics/' + file.name);
+    const file = e.target.files[0];
+    // create storage ref: ex. ('folder_name/file_name)
+    const storageRef = firebase.storage().ref('somethingSpecial_pics/' + file.name);
     // upload file
-    var task = storageRef.put(file);
+    const task = storageRef.put(file);
     //update progress bar
     task.on('state_changed',
+
         function progress(snapshot) {
-            var percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+            const percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             uploader.value = percentage;
         },
         function error(err) {
