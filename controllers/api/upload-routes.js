695d9ac7a204e37e758cee62274a1659b234ffd3
Elysiayn <wendy.lemus.wl@gmail.com>
2021-02-10 19:19:35 -0500

    added file upload front-end to dashboard

--- controllers/api/upload-routes.js
+++ controllers/api/upload-routes.js
@@ -1,21 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Upload } = require('../../models');
-
-// GET /api/upload
-router.get('/', (req, res) => {});
-
-// GET /api/upload/1
-router.get('/:id', (req, res) => {});
-
-// POST /api/upload
-router.post('/', (req, res) => {});
-
-// PUT /api/upload/1
-router.put('/:id', (req, res) => {});
-
-// DELETE /api/upload/1
-router.delete('/:id', (req, res) => {});
+const withAuth = require('../../utils/auth');
 
 
 module.exports = router;
--- models/Upload.js
+++ models/Upload.js
@@ -1,15 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-var admin = require('firebase-admin');
-var app = admin.initializeApp();
+const admin = require('firebase-admin');
+const app = admin.initializeApp();
 
 
-
-app.use((req, res) => {
-    res.status(404).render("404page", {title:"404 not found",
-    uploadstyle: `<link rel="stylesheet" href="/public/stylesheets/upload.css">`});
-});
 
 
 module.exports = Upload;
