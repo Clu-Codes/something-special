1b62c1113afdd636058da01b8f86b8a46a84ae1a
Mila Decker <milacdecker@gmail.com>
2021-02-11 15:43:15 -0500

    added category filter

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -114,6 +114,66 @@ router.get('/post/:id', (req, res) => {
         });
 });
 
+router.get('/category/:category', (req, res) => {
+    Post.findAll({
+        where: {
+            '$category_name$': req.params.category
+        },
+        attributes: [
+            'id',
+            'title',
+            'price',
+            'description',
+            // add images/upload
+            'created_at'
+        ],
+        include: [
+            {
+                model: Message,
+                attributes: [
+                    'id',
+                    'message_text',
+                    'user_id',
+                    'post_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Category,
+                attributes: ['category_name']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) { 
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data and pass to template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            console.log(posts)
+            
+            res.render('homepage', { 
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
