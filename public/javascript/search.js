24ef22a04f8bad95b6f00afc3a7449c3a9c2b6d7
Mila Decker <milacdecker@gmail.com>
2021-02-12 23:49:40 -0500

    dynamically created elements for search results

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -27,7 +27,6 @@ async function handleSearch(event) {
             }
         })
 
-
         const filteredPosts = allPosts.filter(post => {
             return (
                 post.title.toLowerCase().includes(searchTerm) ||
@@ -36,7 +35,60 @@ async function handleSearch(event) {
         })
 
         console.log(filteredPosts)
+        renderResults(filteredPosts)
     };
+};
+
+function renderResults(searchResults) {
+    const allPostsEl = document.querySelector('.all-posts');
+    const searchResultsEl = document.querySelector('.search-results');
+    const searchRowEl = document.querySelector('.search-row');
+
+    allPostsEl.classList.add('hide');
+    searchResultsEl.classList.remove('hide');
+
+    searchResults.forEach(post => {
+        const colEl = document.createElement('div');
+        colEl.classList.add('col');
+        searchRowEl.appendChild(colEl);
+
+        const anchorEl = document.createElement('a');
+        anchorEl.classList.add('card-link');
+        anchorEl.setAttribute('href', `/post/${post.id}`);
+        colEl.appendChild(anchorEl);
+
+        const cardEl = document.createElement('div');
+        cardEl.classList.add('card', 'rounded', 'mt-2', 'mb-3', 'bg-light');
+        colEl.appendChild(cardEl);
+
+        const imgEl = document.createElement('img');
+        imgEl.classList.add('rounded-top');
+        imgEl.setAttribute('src', `${post.image_url}`);
+        cardEl.appendChild(imgEl);
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body', 'rounded', 'bg-light');
+        cardEl.appendChild(cardBody);
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = `$${post.price}`;
+        cardBody.appendChild(cardTitle);
+
+        const cardText = document.createElement('p');
+        cardText.classList.add('card-text');
+        cardText.textContent = `${post.title} in ${post.category.category_name}`;
+        cardBody.appendChild(cardText);
+
+        const cardFooter = document.createElement('div');
+        cardFooter.classList.add('card-footer', 'bg-light');
+        cardBody.append(cardFooter);
+
+        const footerText = document.createElement('small');
+        footerText.classList.add('text-muted');
+        footerText.textContent = `Posted by ${post.user.username}`;
+        cardFooter.appendChild(footerText);
+    });
 };
 
 document.querySelector('#search-form').addEventListener('submit', handleSearch);
